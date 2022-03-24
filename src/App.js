0c257d127f59f34b03703b6bf1dcbde6dfa2ce2f
dawson <dawsonreschke@icloud.com>
2022-03-24 12:53:51 -0700

    refactored application using `React.createContext & useContext`

--- src/App.js
+++ src/App.js
@@ -7,26 +7,31 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+/** Context Imports */
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart,item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			<CartContext.Provider value={{cart}}>
+				<Navigation/>
+				<Route path="/cart">
+					<ShoppingCart/>
+				</Route>
+			</CartContext.Provider>
+			<ProductContext.Provider value={{products,addItem}} >
+				<Route exact path="/">
+					<Products/>
+				</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
